70b4f799fcd6360ea0585dab3b616768ccbbf68b
Torin Stephen <30649929+torin-stephen@users.noreply.github.com>
2024-04-07 15:51:01 +0100

    try 8

---
+++ settingsbuilder.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
+
+# Fetching content from the settings file URL
+settings_url = "https://raw.githubusercontent.com/torin-stephen/FlocusTweaks/main/settings"
+response = requests.get(settings_url)
+settings_content = response.text
+
+# Parsing the settings content
+settings_lines = settings_content.split("\n")
+
+# Extracting content after "Changelog"
+changelog_index = settings_lines.index("Changelog:") + 1
+changelog_content = "\n".join(settings_lines[changelog_index:])
+
+# Fetching content from the HTML file URL
+html_url = "https://raw.githubusercontent.com/torin-stephen/FlocusTweaks/main/settings.html"
+html_response = requests.get(html_url)
+html_content = html_response.text
+
+# Modifying the HTML content
+soup = BeautifulSoup(html_content, "html.parser")
+changelog_section = soup.find("div", class_="mb-4 col-12")
+
+# Replacing changelog content
+changelog_section.clear()
+changelog_section.append(BeautifulSoup(changelog_content, "html.parser"))
+
+# Uploading modified HTML content
+modified_html_content = str(soup)
+with open("modified_settings.html", "w") as file:
+    file.write(modified_html_content)
+
+print("Modified HTML content saved to modified_settings.html")
